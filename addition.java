f9afe8eeb2c9a5148c07494330be52d80aa9b26f
Mohan <muralik1403@gmail.com>
2022-10-17 15:14:32 +0000

    Modified Java addition of two numbers

--- addition.java
+++ addition.java
@@ -1,14 +1,18 @@
-public class JavaExample {
-  public static void main(String[] args) {
-    // two integer variables with values
-    // and a variable "sum" to store the result
-    int num1 = 5, num2 = 15,sum;
+import java.util.Scanner;
+public class AddTwoNumbers2 {
 
-    //calculating the sum of num1 and num2 and
-    //storing the result in the variable sum
-    sum = num1+num2;
-
-    //printing the result
-    System.out.println("Sum of "+num1+" and "+num2+" is: "+sum);
-  }
+    public static void main(String[] args) {
+        
+        int num1, num2, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number: ");
+        num1 = sc.nextInt();
+        
+        System.out.println("Enter Second Number: ");
+        num2 = sc.nextInt();
+        
+        sc.close();
+        sum = num1 + num2;
+        System.out.println("Sum of these numbers: "+sum);
+    }
 }
